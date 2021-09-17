fea98bc70ef2c5a13b65b08c1bfe2f9019e21850
SiposLevente <sipi.levente@gmail.com>
2021-09-17 23:24:32 +0200

    initial commit

---
+++ avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86_64/lib/gcc/avr/4.6.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/tools/hudson/workspace/avr8-gnu-toolchain/src/gcc/configure LDFLAGS=-L/home/tools/hudson/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86_64/lib CPPFLAGS= --target=avr --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --prefix=/home/tools/hudson/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86_64 --libdir=/home/tools/hudson/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86_64/lib --enable-languages=c --with-pkgversion=AVR_Toolchain_3.0_201208171341 --with-bugurl=http://www.atmel.com --with-dwarf2 --enable-doc --disable-shared --disable-libada --disable-libssp --disable-nls --with-mpfr=/home/tools/hudson/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86_64 --with-gmp=/home/tools/hudson/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86_64 --with-mpc=/home/tools/hudson/workspace/avr8-gnu-toolchain/avr8-gnu-toolchain-linux_x86_64";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
